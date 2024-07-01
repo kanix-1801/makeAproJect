bf80fca6a743f0e76bf5ff4cc6a5aec223cb68b1
kanishk singhal <kanishk.singhal1801@gmail.com>
2024-07-01 09:30:10 +0530

    work

---
+++ reactRouterProject/src/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './componentes/Header/Header'
+import Footer from './componentes/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
--- reactRouterProject/src/main.jsx
+++ reactRouterProject/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from './componentes/Home/Home.jsx'
+import AboutUs from './componentes/AboutUs/AboutUs.jsx'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <AboutUs />
+      }
+    ]
+
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
