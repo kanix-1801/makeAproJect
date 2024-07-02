32b892d4add4de50fc285c7f3be167a2bf04fbbe
kanishk singhal <kanishk.singhal1801@gmail.com>
2024-07-02 21:57:25 +0530

    work

--- reactRouterProject/src/componentes/Github/Github.jsx
+++ reactRouterProject/src/componentes/Github/Github.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('https://api.github.com/users/kanix-1801')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setData(data);
+            })
+    }), []
     return (
         <div>
-            Github
+            <div>user name : {data.name}</div>
+            <div>user_id : {data.login}</div>
+            <img src={data.avatar_url} alt="avtar" />
         </div>
     )
 }
